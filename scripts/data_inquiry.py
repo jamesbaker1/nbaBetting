3477a816294a1929d7767749645e20aef63a1480
James Baker <jbaker1@mit.edu>
2019-02-13 00:22:52 -0500

    make neater

--- scripts/data_inquiry.py
+++ scripts/data_inquiry.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 df = pd.read_csv("the_dataaaa.csv")
+
+# Proccess data
 df[["Home Team Wins"]] *= 1
-# print(df.columns)
+df.rename(columns={'Home Team Wins':'HOME_TEAM_WINS'}, inplace=True)
+df[['HOME_TEAM_WINS']] = df[['HOME_TEAM_WINS']].astype(float)
+
+
 corrmat = df.corr()
 # f, ax = plt.subplots(figsize=(20,18))
 # sns.heatmap(corrmat, vmax=.8, square=True)
@@ -14,7 +19,7 @@ corrmat = df.corr()
 #team result
 
 k = 9
-cols = corrmat.nlargest(k, 'Home Team Wins')['Home Team Wins'].index
+cols = corrmat.nlargest(k, "HOME_TEAM_WINS")["HOME_TEAM_WINS"].index
 f, ax = plt.subplots(figsize=(10,6))
 cm = np.corrcoef(df[cols].values.T)
 sns.set(font_scale=1.25)
--- scripts/predict.py
+++ scripts/predict.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn import svm
 from sklearn.svm import LinearSVC
 from sklearn.neighbors import KNeighborsClassifier
@@ -12,12 +13,20 @@ import graphviz
 from sklearn import tree
 
 
+# Prepare data
+df = pd.read_csv("the_dataaaa.csv")
+df[["Home Team Wins"]] *= 1
+df.rename(columns={'Home Team Wins':'HOME_TEAM_WINS'}, inplace=True)
+df[['HOME_TEAM_WINS']] = df[['HOME_TEAM_WINS']].astype(float)
+
 #prepare x and y
 
-feature_cols = ['opptPTS', 'teamDrtg', 'teamPF', 'teamTO', 'teamORB', 'teamFGA']
+# feature_cols = ['opptPTS', 'teamDrtg', 'teamPF', 'teamTO', 'teamORB', 'teamFGA']
+feature_cols = ['HOME_W_PCT', 'HOME_FG_PCT', 'HOME_FG3_PCT', 'HOME_OREB', 'HOME_DREB', 'HOME_AST', 'HOME_TOV', 'HOME_STL', 'HOME_BLK', 'HOME_PTS', 'AWAY_DATE', 'AWAY_W_PCT', 'AWAY_FG_PCT', 'AWAY_FG3_PCT', 'AWAY_OREB','AWAY_DREB', 'AWAY_AST', 'AWAY_TOV', 'AWAY_STL', 'AWAY_BLK', 'AWAY_PTS']
 x = df[feature_cols]
-y = df['teamRslt']
-x.head()
+y = df['HOME_TEAM_WINS']
+# print(x.head())
+
 
 #train test split, standardize data
 
@@ -28,8 +37,9 @@ x_train, x_test, y_train, y_test = train_test_split(x, y , test_size=0.4, random
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(x_train, y_train)
 pred = knn.predict(x_test)
-print(metrics.accuracy_score(y_test, pred))
+# print(metrics.accuracy_score(y_test, pred))
 print(knn.predict_proba(x_test))
+
 
 #linear svm
 
@@ -38,7 +48,6 @@ clf.fit(x_train, y_train)
 print(clf.coef_)
 print(clf.intercept_)
 pred = (clf.predict(x_test))
-#print(pred)
 print(metrics.accuracy_score(y_test, pred))
 
 
@@ -48,8 +57,6 @@ clf = RandomForestClassifier()
 clf.fit(x_train, y_train)
 print(clf.feature_importances_)
 pred = clf.predict(x_test)
-#print(pred)
-#print(clf.predict_proba(x_test))
 print(metrics.accuracy_score(y_test, pred))
 
 # Gradient Treee Boosting
